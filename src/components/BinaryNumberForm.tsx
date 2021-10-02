62f26aaa2d04d59081d487b2db55690bd4b97004
cthacker-udel <cthacker@udel.edu>
2021-10-01 23:25:18 -0400

    Added Button and Display for converted number, form input can only be 0s and 1s and the button converts the form input to decimal and displays it in the box

--- src/components/BinaryNumberForm.tsx
+++ src/components/BinaryNumberForm.tsx
@@ -1,18 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form } from 'react-bootstrap';
-import { MouseEvent } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+
 
 export function BinaryNumberForm(props: { value: string }): JSX.Element {
-	const BinaryNumberFormOnChange = (event: MouseEvent): void => {
+	const [displayValue, setDisplayValue] = useState<string>(props.value);
+	const [convertedValue, setConvertedValue] = useState<string>('');
+	const BinaryNumberFormOnChange = (event: React.FormEvent<HTMLElement>): void => {
 		const changedElement: HTMLInputElement = event.target as HTMLInputElement;
 		const input: string = changedElement.value;
 		if (!input.split('').every(e => (e === '0' || e === '1'))) {
 			let tmpInput: string = input;
 			tmpInput = tmpInput.substring(0, tmpInput.length - 1);
-			props.value = tmpInput;
+			setDisplayValue(tmpInput);
 		} else {
-			props.value = input;
+			setDisplayValue(input);
 		}
+	};
+
+	const conversionClick = () => {
+		const binaryNumber: string = displayValue;
+		setConvertedValue(String(parseInt(binaryNumber, 2)));
 	};
 
 
@@ -24,7 +32,20 @@ export function BinaryNumberForm(props: { value: string }): JSX.Element {
 				<Form.Group className="mb-3" controlId="basicNumberContainer">
 
 					<Form.Label>Enter binary number in to be converted to decimal</Form.Label>
-					<Form.Control type="text" value={ props.value } style={{ textAlign: 'center' }} onChange={ BinaryNumberFormOnChange } ></Form.Control>
+					<Form.Control type="text" value={ displayValue } style={{ textAlign: 'center' }} onChange={ BinaryNumberFormOnChange } ></Form.Control>
+
+				</Form.Group>
+				<Button variant="primary" onClick={ conversionClick }>Convert to Decimal</Button>
+
+			</Form>
+			<br />
+			<br />
+			<Form>
+
+				<Form.Group className="converted result" controlId="basicResultContainer">
+
+					<Form.Label>Converted number</Form.Label>
+					<Form.Control type="text" value={ convertedValue } style={{ textAlign: 'center' }} readOnly ></Form.Control>
 
 				</Form.Group>
 
@@ -33,5 +54,3 @@ export function BinaryNumberForm(props: { value: string }): JSX.Element {
 		</>
 	);
 }
-
-export default BinaryNumberForm;
